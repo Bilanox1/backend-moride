cdca7c1389b1e9f2e4369928909abf8052d59d46
Bilanox1 <aurabilanox@gmail.com>
2025-01-26 18:05:57 +0100

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule, InjectConnection } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // Import ConfigModule and ConfigService
+import { Connection } from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    // Load environment variables
+    ConfigModule.forRoot({
+      isGlobal: true, // Make the ConfigModule global
+      envFilePath: '.env', // Specify the path to your .env file
+    }),
+    // Use ConfigService to get the MongoDB URI
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], // Import ConfigModule
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'), // Get the MongoDB URI from .env
+      }),
+      inject: [ConfigService], // Inject ConfigService
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(@InjectConnection() private readonly connection: Connection) {
+    this.connection.on('connected', () => {
+      console.log('Successfully connected to MongoDB database');
+    });
+
+    this.connection.on('error', (error) => {
+      console.error('Failed to connect to MongoDB database:', error);
+    });
+  }
+}
