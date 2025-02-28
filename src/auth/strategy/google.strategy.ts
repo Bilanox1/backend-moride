5fe52c60574b340c5ee0b19089d49feee339cb56
Bilanox1 <aurabilanox@gmail.com>
2025-02-28 13:27:13 +0000

    Add files via upload

--- src/auth/strategy/google.strategy.ts
+++ src/auth/strategy/google.strategy.ts
@@ -8,7 +8,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `http://localhost:3000/api/v1/auth/google/callback`,
+      callbackURL: `https://api-moride-git-main-bilanox1s-projects.vercel.app/api/v1/auth/google/callback`,
       scope: ['profile', 'email'],
     });
   }
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'https://accounts.google.com/',
-      'https://bilal-ez-zaim.github.io',
-    ],
+    origin: ['https://accounts.google.com/', 'https://bilal-ez-zaim.github.io'],
     methods: 'GET,POST,PUT,DELETE,PATCH',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
