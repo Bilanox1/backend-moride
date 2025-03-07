46638dae33f1dc5f67841d436c5d576ed2142f83
Bilanox1 <aurabilanox@gmail.com>
2025-03-07 15:01:17 +0000

    Delete Pricing directory

--- Pricing/dto/update-pricing.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePricingDto } from './create-pricing.dto';
-
-export class UpdateBookingDto extends PartialType(CreatePricingDto) {}
