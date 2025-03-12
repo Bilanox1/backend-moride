602bc8f1a9efb52f1239e0a3c986bf74a3f9e8ac
Bilanox1 <aurabilanox@gmail.com>
2025-03-12 11:32:17 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['*'],
+    origin: ['https://moride.vercel.app'],
     methods: 'GET,POST,PUT,DELETE,PATCH',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true, // This should be true only if cookies or credentials are sent
